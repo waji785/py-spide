6a1dcad60643cb5981fba84b0926b86f9891fa21
unknown <2462177270@qq.com>
2021-01-24 22:57:35 +0800

    weather soider mode,I ntl XML

--- main/website crawl.py
+++ main/website crawl.py
@@ -1,18 +1,61 @@
-import scrapy
-class BossjobSpider(scrapy.Spider):
-    name = ""
-    allowed_domains = []
-    current_page = 1
-    max_page = 10
-    start_url ={
-        "https://www.zhipin.com/c101020100-p100101/?ka=sel-city-101020100"
+import requests
+import wxpy
+from lxml import etree
+def getweather():
 
-    }
-    custom_settings = {
-        "DEFAULT_REQUEST_HEADERS":{
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
-            'Referer': 'https://www.zhipin.com/',
-        }
-    }
+    r = requests.get()
+    tree = etree.HTML(r.text)
+    today = tree.xpath('//div[@class="row2 row2-1"]/a/text()')[0]
+    weather = tree.xpath('//p[@class="des"]/text()')[1]
+    wind = tree.xpath('//p[@class="wind"]/text()')[1]
+    quality = tree.xpath('//span[@class="liv-text"]/a/em/text()')[0]
+    rank = tree.xpath('//span[@class="liv-img liv-img-cor1"]/text()')[0]
+    high = tree.xpath('//div[@class="r-temp"]/@data-high')[0].split(',')[1]
+    low = tree.xpath('//div[@class="r-temp"]/@data-low')[0].split(',')[1]
+    content = '早上好，亲爱的！\n今日份的天气请注意查看喔~\n今天是：'+today+\
+              '\n天气：'+weather+'\n风级：'+wind+'\n最高温度:'+high+\
+              '\n最低温度:'+low+'\n空气质量指数:'+quality+' 等级：'+rank
+    print(content)
+    return content
+
+
+# def main():
+#     message = getWeather()
+#     print('成功获取天气信息')
+
+    #    # 参数hotReload=True实现保持微信网页版登陆状态，下次发送无需再次扫码
+    #    itchat.auto_login()
+    #    users=itchat.search_friends('')
+    #    print(users)
+    #    userName=users[0]['UserName']
+    #    ret=itchat.send(msg = message, toUserName = userName)
+    #    if ret:
+    #        print("成功发送")
+    #    else:
+    #        print("发送失败")
+    #    time.sleep(3)
+    #    itchat.logout()
+def main():
+    message = getweather()
+    print('成功获取天气信息')
+
+    #    # 参数hotReload=True实现保持微信网页版登陆状态，下次发送无需再次扫码
+    #    itchat.auto_login()
+    #    users=itchat.search_friends('')
+    #    print(users)
+    #    userName=users[0]['UserName']
+    #    ret=itchat.send(msg = message, toUserName = userName)
+    #    if ret:
+    #        print("成功发送")
+    #    else:
+    #        print("发送失败")
+    #    time.sleep(3)
+    #    itchat.logout()
+
+    bot = wxpy.Bot()
+    my_friend = bot.friends().search('Snall')[0]
+    my_friend.send(message[0])
+
+
+if __name__ == '__main__':
+    main()
